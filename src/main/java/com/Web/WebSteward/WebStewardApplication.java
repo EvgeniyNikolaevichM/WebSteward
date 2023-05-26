955a65cafef9c994407b0a6835923e9f7ccabd4a
Admin <apbxochu@gmail.com>
2023-05-27 00:31:19 +0400

    Initial commit

---
+++ src/main/java/com/Web/WebSteward/WebStewardApplication.java
@@ -0,0 +1,13 @@
+package com.Web.WebSteward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStewardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStewardApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/Web/WebSteward/interfaces/homeInterface.java
@@ -0,0 +1,8 @@
+package com.Web.WebSteward.interfaces;
+
+import com.Web.WebSteward.models.home;
+import org.springframework.data.repository.CrudRepository;
+
+public interface homeInterface extends CrudRepository<home, Long>{
+
+}
